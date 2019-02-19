095ee0136736a17a9189af92ac3ed40e680850cf
Ilya Ostroukhov <surgatus@gmail.com>
2019-02-19 20:24:48 +0300

    init2

--- main.go
+++ main.go
@@ -2,11 +2,11 @@
 package main
 
 import (
-	"crypto/md5"
-	"encoding/hex"
+	//	"crypto/md5"
+	//	"encoding/hex"
 	"hash"
-	"io"
-	"log"
+	//	"io"
+	//	"log"
 	"strings"
 
 	//	"errors"
@@ -24,16 +24,39 @@ import (
 //	wordPtr := flag.String("word", "foo", "a string")
 //}
 
+type operationType int
+
+const (
+	COPY operationType = 0 + iota
+	RENAME
+	REMOVE
+)
+
+type operation struct {
+	srcPath  string
+	destPath string
+	otype    operationType
+}
+
+func (o operationType) String() string {
+	return [...]string{"COPY", "RENAME", "REMOVE"}[o]
+}
+
 type pathInfo struct {
 	fullPath string
 	path     string
 	fileInfo os.FileInfo
 	hash     hash.Hash
+	readed   bool
 }
 
-func loadPathInfos(location string) map[string][]*pathInfo {
+func (p *pathInfo) compare(toP *pathInfo) bool {
+	return p.fileInfo.Size() == toP.fileInfo.Size()
+}
 
-	storage := make(map[string][]*pathInfo)
+func loadPathInfos(location string) map[string]*pathInfo {
+
+	storage := make(map[string]*pathInfo)
 
 	var visit = func(fullPath string, fileInfo os.FileInfo, err error) error {
 
@@ -41,26 +64,30 @@ func loadPathInfos(location string) map[string][]*pathInfo {
 		info.fullPath = fullPath
 		info.path = strings.Replace(fullPath, location, "", -1)
 		info.fileInfo = fileInfo
+		info.readed = false
 
 		if !fileInfo.IsDir() {
 
-			f, err := os.Open(fullPath)
-			if err != nil {
-				return nil
-				log.Fatal(err)
-			}
-			defer f.Close()
+			//			f, err := os.Open(fullPath)
+			//			if err != nil {
+			//				return nil
+			//				log.Fatal(err)
+			//			}
+			//			defer f.Close()
 
-			h := md5.New()
-			if _, err := io.Copy(h, f); err != nil {
-				return nil
-				log.Fatal(err)
-			}
+			//			h := md5.New()
+			//			if _, err := io.Copy(h, f); err != nil {
+			//				return nil
+			//				log.Fatal(err)
+			//			}
 
-			md5String := hex.EncodeToString(h.Sum(nil))
+			//			md5String := hex.EncodeToString(h.Sum(nil))
+			//
+			//			if _, ok := storage[md5String]; !ok {
+			//				storage[md5String] = make(map[string]*pathInfo)
+			//			}
 
-			storage[md5String] = append(storage[md5String], info)
-			//			storage[md5String][] = info
+			storage[info.path] = info
 		}
 
 		return nil
@@ -73,34 +100,75 @@ func loadPathInfos(location string) map[string][]*pathInfo {
 
 func main() {
 	flag.Parse()
+	//	db, _ := buntdb.Open(":memory:")
+	//
+	//	err := db.Update(func(tx *buntdb.Tx) error {
+	//		_, _, err := tx.Set("mykey", "myvalue", nil)
+	//		return err
+	//	})
 
 	args := flag.Args()
 	len := len(args)
 	if len != 2 {
 		panic("Not fount from and to")
 	}
-
-	from := args[0]
-
-	fromPathInfos := loadPathInfos(from)
-
 	to := args[1]
 	toPathInfos := loadPathInfos(to)
 
-	for h, fromPathInfo := range fromPathInfos {
-		//		fmt.Println(h)
-		if toPathInfo, ok := toPathInfos[h]; ok {
-			fmt.Println(fromPathInfo.path + "   " + toPathInfo.path)
-			if fromPathInfo.path == toPathInfo.path {
-				fmt.Println("Ok")
+	from := args[0]
+	fromPathInfos := loadPathInfos(from)
+
+	operations := make([]operation, 0)
+	for fromPath, fromPathInfo := range fromPathInfos {
+		if toPathInfo, ok := toPathInfos[fromPath]; ok {
+			if fromPathInfo.compare(toPathInfo) {
+				fromPathInfo.readed = true
+				toPathInfo.readed = true
+
+				continue
 			}
+		}
+
+		for toPath, toPathInfo := range toPathInfos {
+			if fromPathInfo.compare(toPathInfo) {
+
+				fromPathInfo.readed = true
+				toPathInfo.readed = true
+
+				op := new(operation)
+				op.srcPath = fromPath
+				op.destPath = toPath
+				op.otype = RENAME
+
+				operations = append(operations, *op)
+				continue
+			}
+		}
+
+		if fromPathInfo.readed == false {
+			fromPathInfo.readed = true
+
+			op := new(operation)
+			op.srcPath = fromPath
+			op.destPath = fromPath
+			op.otype = COPY
+
+			operations = append(operations, *op)
 		}
 	}
 
-	//    fromFiles := map[string]*pathInfo
-	//	err := filepath.Walk(from, loadPathInfos(&fromFiles))
-	//	if err != nil {
-	//		fmt.Printf("filepath.Walk() returned %v\n", err)
-	//	}
+	for toPath, toPathInfo := range toPathInfos {
+		if toPathInfo.readed == false {
+			op := new(operation)
+			op.srcPath = ""
+			op.destPath = toPath
+			op.otype = REMOVE
 
+			operations = append(operations, *op)
+		}
+	}
+
+	for _, operation := range operations {
+		fmt.Printf("%s -> %s : %s\n", operation.srcPath, operation.destPath, operation.otype.String())
+	}
 }
